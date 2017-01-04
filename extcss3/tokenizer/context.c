95a85f329e30301c0520f793d25fb996d436c66b
Alex Schneider <alex.schneider@sevenval.com>
2017-01-04 09:23:56 +0100

    Context updater more secure and stable

--- extcss3/tokenizer/context.c
+++ extcss3/tokenizer/context.c
@@ -3,8 +3,24 @@
 
 /* ==================================================================================================== */
 
+void inline _extcss3_ctxt_parent(extcss3_intern *intern)
+{
+	extcss3_ctxt *prev;
+
+	if ((intern->last_ctxt != NULL) && (intern->last_ctxt->prev != NULL)) {
+		prev = intern->last_ctxt->prev;
+		prev->next = NULL;
+		extcss3_release_ctxt(intern->last_ctxt, false);
+		intern->last_ctxt = prev;
+	}
+}
+
 int inline extcss3_ctxt_update(extcss3_intern *intern)
 {
+	if ((intern == NULL) || (intern->last_token == NULL) || (intern->last_ctxt == NULL)) {
+		return EXTCSS3_ERR_NULL_PTR;
+	}
+
 	switch (intern->last_token->type) {
 		case EXTCSS3_TYPE_AT_KEYWORD:
 		case EXTCSS3_TYPE_FUNCTION:
@@ -26,29 +42,29 @@ int inline extcss3_ctxt_update(extcss3_intern *intern)
 		case EXTCSS3_TYPE_BR_RC:
 		case EXTCSS3_TYPE_BR_SC:
 		{
-			extcss3_ctxt_parent(intern);
+			_extcss3_ctxt_parent(intern);
 
 			break;
 		}
 		case EXTCSS3_TYPE_BR_CC:
 		{
 			if (
-				intern->last_ctxt->prev &&
-				intern->last_ctxt->prev->token &&
+				(intern->last_ctxt->prev != NULL) &&
+				(intern->last_ctxt->prev->token != NULL) &&
 				(intern->last_ctxt->prev->token->type == EXTCSS3_TYPE_AT_KEYWORD)
 			) {
-				extcss3_ctxt_parent(intern);
-				extcss3_ctxt_parent(intern);
-			} else if (intern->last_ctxt) {
-				extcss3_ctxt_parent(intern);
+				_extcss3_ctxt_parent(intern);
+				_extcss3_ctxt_parent(intern);
+			} else {
+				_extcss3_ctxt_parent(intern);
 			}
 
 			break;
 		}
 		case EXTCSS3_TYPE_SEMICOLON:
 		{
-			if (intern->last_ctxt->token && (intern->last_ctxt->token->type == EXTCSS3_TYPE_AT_KEYWORD)) {
-				extcss3_ctxt_parent(intern);
+			if ((intern->last_ctxt->token != NULL) && (intern->last_ctxt->token->type == EXTCSS3_TYPE_AT_KEYWORD)) {
+				_extcss3_ctxt_parent(intern);
 			}
 
 			break;
@@ -56,16 +72,4 @@ int inline extcss3_ctxt_update(extcss3_intern *intern)
 	}
 
 	return 0;
-}
-
-void inline extcss3_ctxt_parent(extcss3_intern *intern)
-{
-	extcss3_ctxt *prev;
-
-	if (intern->last_ctxt->prev) {
-		prev = intern->last_ctxt->prev;
-		prev->next = NULL;
-		extcss3_release_ctxt(intern->last_ctxt, false);
-		intern->last_ctxt = prev;
-	}
 }
--- extcss3/tokenizer/context.h
+++ extcss3/tokenizer/context.h
@@ -6,6 +6,5 @@
 /* ==================================================================================================== */
 
 int extcss3_ctxt_update(extcss3_intern *intern);
-void extcss3_ctxt_parent(extcss3_intern *intern);
 
 #endif /* EXTCSS3_TOKENIZER_CONTEXT_H */
